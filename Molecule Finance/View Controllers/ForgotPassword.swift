4417d75bc4b373a5c5315e000b78e20da02c722e
Rushabh <nagori.rushabh@gmail.com>
2019-07-02 10:21:54 -0700

    Complete forgot password page

---
+++ Molecule Finance/View Controllers/ForgotPassword.swift
@@ -0,0 +1,41 @@
+//
+//  ForgotPassword.swift
+//  Molecule Finance
+//
+//  Created by Rushabh Nagori on 7/2/19.
+//  Copyright © 2019 Rushabh Nagori. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import FirebaseAuth
+import Firebase
+import SVProgressHUD
+import PopupDialog
+
+class ForgotPassword: UIViewController {
+    @IBOutlet weak var email: UITextField!
+    @IBAction func resetPassword(_ sender: Any) {
+        SVProgressHUD.show()
+        Auth.auth().sendPasswordReset(withEmail: email.text!) { (error) in
+            if error != nil {
+                SVProgressHUD.dismiss()
+                let popup = PopupDialog(title: "Invalid Email", message: "Make sure you are a registered user and have entered the correct email address")
+                let okayButton = DefaultButton(title: "Okay", action: {
+                })
+                popup.addButton(okayButton)
+                PopupDialogContainerView.appearance().backgroundColor = UIColor(red: 0.23, green: 0.23, blue: 0.23, alpha: 1.00)
+                let buttonAppearence = DefaultButton.appearance()
+                buttonAppearence.titleColor = .white
+                let messageAppearence = PopupDialogDefaultView.appearance()
+                messageAppearence.titleColor = .white
+                messageAppearence.messageColor = .lightGray
+                self.present(popup, animated: true, completion: nil)
+            }
+            else{
+                SVProgressHUD.dismiss()
+                self.performSegue(withIdentifier: "forgotPassword", sender: self)
+            }
+        }
+    }
+}
--- Molecule Finance/View Controllers/Signin.swift
+++ Molecule Finance/View Controllers/Signin.swift
@@ -81,4 +81,6 @@ class Signin: UIViewController {
             }
         }
     }
+    
+    
 }
